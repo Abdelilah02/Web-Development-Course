abfd09e85864a9f5b95c0227b56621f16067ab0c
Abdelilah02 <68647904+Abdelilah02@users.noreply.github.com>
2020-08-11 12:42:04 +0200

    Add files via upload

---
+++ Module4-Solutions/SpeakHello.js
@@ -0,0 +1,18 @@
+
+(function(window){
+
+var helloSpeaker = new Object;
+helloSpeaker.speak= function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  var speakWord = "Hello";
+
+  window.helloSpeaker= helloSpeaker;
+
+
+
+
+})(window);
+
+
